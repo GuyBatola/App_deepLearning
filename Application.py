736db286844a160f878f2b06bc9bbfafed44f987
GuyBatola <167473003+GuyBatola@users.noreply.github.com>
2024-04-20 17:35:14 +0200

    Update Application.py

--- Application.py
+++ Application.py
@@ -3,13 +3,14 @@ from sklearn.feature_extraction.text import TfidfVectorizer
 import pickle
 
 # Liste des modèles disponibles
-MODELS = ['Arbre de décision', 'Forêt aléatoire', 'XGBoost', 'Réseaux de neurones avec Word Embedding', 'Réseau de neurones récurrents']
+MODELS = ['Arbre de décision', 'Forêt aléatoire', 'XGBoost', 'Réseaux de neurones', 'Réseau de neurones récurrents']
 
 # Chargement du vocabulaire et des modèles pré-entrainés
 vectorize = pickle.load(open("vectorizer.pkl", "rb"))
     
 tree = pickle.load(open("tree.pkl", "rb"))
-
+xgb = pickle.load(open("xgb.pkl", "rb"))
+neurone = pickle.load(open("tree.pkl", "rb"))
 # Sélection du modèle
 selected_model = st.sidebar.selectbox("Sélectionnez un modèle", MODELS)
 
@@ -23,13 +24,13 @@ elif selected_model == 'Forêt aléatoire':
     st.sidebar.markdown("""
     La forêt aléatoire est un ensemble d'arbres de décision. Chaque arbre est construit sur un sous-ensemble aléatoire des données d'entraînement et utilise une sous-ensemble aléatoire des caractéristiques pour la division.
     """)
-    model = tree
+    model = xgb
 elif selected_model == 'XGBoost':
     st.sidebar.markdown("""
     XGBoost est une implémentation optimisée du gradient boosting. Il combine plusieurs modèles faibles pour former un modèle fort en utilisant le gradient de la fonction de perte.
     """)
-    model = tree
-elif selected_model == 'Réseau de neurones avec Word Embedding':
+    model = xgb
+elif selected_model == 'Réseau de neurones':
     st.sidebar.markdown("""
     Le réseau de neurones avec Word Embedding utilise une couche d'embedding pour convertir les mots en vecteurs denses avant de les passer au réseau de neurones. Cela permet au modèle de capturer les relations sémantiques entre les mots.
     """)
@@ -48,8 +49,24 @@ comment = st.text_area("Saisissez votre commentaire ici")
 # Bouton pour soumettre le commentaire
 if st.button("Valider"):
     if comment:
-        a = vectorize.transform([comment])
-        sentiment = model.predict(a)
-        st.write("Sentiment prédit : ", sentiment)
+        # tree
+        if selected_model == 'tree':
+            a = vectorize.transform([comment])
+            sentiment = model.predict(a)
+        # RF et XGB
+        elif selected_model == 'Forêt aléatoire' or selected_model == 'XGBoost' :
+            a = vectorize.transform([comment])
+            sentiment = model.predict(a)
+            if sentiment=="2" :
+                sentiment2 = "Positif"
+                st.write("Sentiment prédit : ", sentiment2)
+            elif sentiment=="1" :
+                sentiment2 = "Negatif"
+                st.write("Sentiment prédit : ", sentiment2)
+            else :
+                sentiment2 = "Mitige"
+                st.write("Sentiment prédit : ", sentiment2)
+        else:
+             modelneurone.predict(a.toarray()).argmax(-1)
     else:
         st.warning("Veuillez saisir un commentaire.")
